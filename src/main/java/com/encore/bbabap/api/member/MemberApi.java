87e8ab7c368459c46bbe1a4d3c74ee45b14723c9
dongwook <hsw50108@gmail.com>
2024-03-15 23:43:02 +0900

    test : git branch

---
+++ src/main/java/com/encore/bbabap/api/member/MemberApi.java
@@ -0,0 +1,13 @@
+package com.encore.bbabap.api.member;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberApi {
+
+    @GetMapping("/member")
+    public String test() {
+        System.out.println("git branch test 입니다.");
+    }
+}
