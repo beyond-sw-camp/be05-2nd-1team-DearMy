42f53f509d9352fe67ec51ac46c4b28c09eae4e7
chan-nni <rkdcksal02@gmail.com>
2024-03-22 04:38:53 +0900

    fix: 사용자 인증 후 댓글 수정/삭제

--- src/main/java/com/encore/bbabap/api/comment/request/CommentRequestDTO.java
+++ src/main/java/com/encore/bbabap/api/comment/request/CommentRequestDTO.java
@@ -7,6 +7,5 @@ import lombok.Setter;
 @Setter
 public class CommentRequestDTO {
     private String content;
-    private String email; // 사용자 이메일
-    private Long boardId; // 게시물 ID
+    private Long boardId;
 }
