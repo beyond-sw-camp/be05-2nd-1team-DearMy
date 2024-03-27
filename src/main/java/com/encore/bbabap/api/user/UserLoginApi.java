066727a37dc95e6a312412cbde42fa22a5286965
chan-nni <rkdcksal02@gmail.com>
2024-03-27 15:18:49 +0900

    로그인 API

--- src/main/java/com/encore/bbabap/api/user/UserLoginApi.java
+++ src/main/java/com/encore/bbabap/api/user/UserLoginApi.java
@@ -1,16 +1,28 @@
 package com.encore.bbabap.api.user;
 
 import com.encore.bbabap.api.user.request.LoginUserRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "LoginController", description = "로그인 기능")
 public class UserLoginApi {
 
-    // TODO : 지금은 필요없는 코드  -> rest api 형식으로 커스텀 하기
     @PostMapping("/login")
+    @Operation(summary = "사용자 로그인", description = "사용자 로그인을 처리하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "401", description = "로그인 실패")
+    })
     public String login(@RequestBody LoginUserRequest request) {
+
+
+
         return request.getPassword();
     }
 
